a6871f7dcef6c51c1ef397cafc8717a9e962c6a6
Vndrvs <99025350+Vndrvs@users.noreply.github.com>
2026-01-28 13:53:25 +0800

    chen done, pinia setup in progress

--- app/composables/chenCalculator.ts
+++ app/composables/chenCalculator.ts
@@ -1,21 +1,7 @@
-// The Chen formula.
-// Score your highest card only. Do not add any points for your lower card.
-// A = 10 points.
-// K = 8 points.
-// Q = 7 points.
-// J = 6 points.
-// 10 to 2 = 1/2 of card value. (e.g. a 6 would be worth 3 points)
-// Multiply pairs by 2 of one card’s value. However, minimum score for a pair is 5.
-// (e.g. KK = 16 points, 77 = 7 points, 22 = 5 points)
-// Add 2 points if cards are suited.
-// Subtract points if their is a gap between the two cards.
-// No gap = -0 points.
-// 1 card gap = -1 points.
-// 2 card gap = -2 points.
-// 3 card gap = -4 points.
-// 4 card gap or more = -5 points. (Aces are high this step, so hands like A2, A3 etc. have a 4+ gap.)
-// Add 1 point if there is a 0 or 1 card gap and both cards are lower than a Q. (e.g. JT, 75, 32 etc, this bonus point does not apply to pocket pairs)
-// Round half point scores up. (e.g. 7.5 rounds up to 8)
+// The Chen formula is the invention and intellectual property of Bill Chen, the author of Mathematics of Poker
+// https://www.amazon.com/gp/product/1886070253?ie=UTF8&tag=thpoba-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1886070253
+// which was featured in the book 'Hold'em Excellence: From Beginner to Winner' by Lou Krieger
+// https://www.amazon.com/gp/product/1886070148?ie=UTF8&tag=thpoba-20&linkCode=as2&camp=1789&creative=9325&creativeASIN=1886070148
 
 import { RANKS, useDeck } from '@/composables/useDeck';
 
@@ -42,6 +28,7 @@ interface ComboAnalysis {
     isValid: boolean;
 }
 
+// helper to determine whether a combo is valid, suited, paired
 export const getComboInfo = (combo: CardPair): ComboAnalysis => {
 	const c1 = getCardDetail(combo.id1);
 	const c2 = getCardDetail(combo.id2);
@@ -76,7 +63,6 @@ export const gapScorer = (index1: number, index2: number): number => {
   return 5;
 }
 
-// Step 1: Scoring higher card
 export const chenScore = (combo: CardPair): number => {
 	const c1 = getCardDetail(combo.id1);
 	const c2 = getCardDetail(combo.id2);
@@ -94,7 +80,7 @@ export const chenScore = (combo: CardPair): number => {
 	const higherIndex = Math.max(index1, index2)
 	const rankCharacter = RANKS[higherIndex]!
 
-	// we score it - A to J is given by the algorithm rules
+	// we score it - A to J score is given by the algorithm rules
 	// lower ranks have a score of their numerical value divided by 2
 	let score;
 
@@ -102,16 +88,18 @@ export const chenScore = (combo: CardPair): number => {
 		score = CHEN_SCORE[rankCharacter]
 	} else {
 		score = parseInt(rankCharacter) / 2;
-	}
+	} // now we have the starter score value
 
 	if (comboInfo.isPair) {
-		score = Math.max(5, score! * 2);
+		score = Math.max(5, score! * 2); // if pair, we multiply it by 2
 	} else if(comboInfo.isSuited) {
-		score! += 2;
+		score! += 2; // if suited, we add 2 to the score
 	}
 
+	// if the combo has a gap between the cards, we penalize it by deducting a certain number (handled earlier)
 	score! -= gapPenalty;
 
+	// bonus point for specific combos
 	if (
 		!comboInfo.isPair &&
 		gapPenalty <= 1 &&
@@ -121,5 +109,6 @@ export const chenScore = (combo: CardPair): number => {
 		score! +=1;
 	}
 
+	// we round up the result
 	return Math.ceil(score!);
 }
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,6 +20,7 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxtjs/color-mode',
     'nuxt-headlessui',
+    '@pinia/nuxt',
   ],
   headlessui: {
     prefix: 'H'
